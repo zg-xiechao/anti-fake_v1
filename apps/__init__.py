a03378353a489323e77d9800f7d86139106c94c1
xiechao <994132001@qq.com>
2018-12-25 15:10:35 +0800

    第一次合并(小修改)

--- apps/__init__.py
+++ apps/__init__.py
@@ -3,29 +3,29 @@ from flask_session import Session
 
 
 # 数据库
-def register_db(app: Flask):
-	from apps.models import db
-	db.init_app(app=app)
+def register_db(app):
+    from apps.models import db
+    db.init_app(app=app)
 
 
 # 蓝图
-def register_cms_bp(app: Flask):
-	from apps.cms import cms_bp
-	app.register_blueprint(cms_bp)
+def register_cms_bp(app):
+    from apps.cms import cms_bp
+    app.register_blueprint(cms_bp)
 
 
 # 创建app
 def create_app():
-	app = Flask(__name__, static_url_path='/static/', static_folder='./static')
-	app.config.from_object('apps.settings.DevConfig')
+    app = Flask(__name__, static_url_path='/static/', static_folder='./static')
+    app.config.from_object('apps.settings.DevConfig')
 
-	# session
-	Session(app=app)
+    # session
+    Session(app=app)
 
-	# 数据库
-	register_db(app=app)
+    # 数据库
+    register_db(app=app)
 
-	# 蓝图
-	register_cms_bp(app=app)
+    # 蓝图
+    register_cms_bp(app=app)
 
-	return app
+    return app
